50502d5077cbb5f8bc4ad9e780abee6e4ded5fe7
Clay Webb <clay_webb@outlook.com>
2022-03-15 15:20:31 -0500

    Added code comments to LandingPage.jsx to improve structure and understanding of it's functionality

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -7,17 +7,23 @@ import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingPage() {
 
-  // Welcome Statement
+  //=================<START STATE VARIABLES>====================================================================
+      // Welcome Statement
   const [heading, setHeading] = useState('Welcome to Hey Bill W.');
-  // Project Overview
+      // Project Overview
   const [firstParagraph, setFirstParagraph] = useState(`During the Covid-19 lockdown in March of 2020, everyone was affected to some extent from the stay-at-home order, especially the A.A. community. Which relies on mutual support from members of the community in order to follow the program. That's why I believe in this day and age, a virtual recovery experience is something that needs more attention. With my app, Hey Bill W, in honor of the founder of Alcoholics Anonymous. Those who are in A.A. will log in, and be greeted with a daily prayer and reminders of what they have to do that day. Be able to follow and update the choices they are making during their 12 Steps. Have access to The Big Book, and an interactive map of their location, as well as the location of other users when given their unique key. There will also be a page regarding the history of A.A. and Bill W, information about the program, and contact information for local treatment centers.`)
+  //------<END STATE VARIABLES>---------------------------------------------------------------------------
 
+  // Invoke useHistory
   const history = useHistory();
 
+  //=================<CLICK LISTENER>===========================================
+      // When the "Login" button is clicked, brings the user to '/login' view
   const onLogin = (event) => {
     history.push('/login');
-  };
+  }; //------<END CLICK LISTENER>-------------------------------
 
+  //=================<START RETURN>====================================================================
   return (
     <div className="container">
       <h2>{heading}</h2>
@@ -33,14 +39,14 @@ function LandingPage() {
 
           <center>
             <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
-        </div>
-      </div>
-    </div>
-  );
-}
+            <button className="btn btn_sizeSm" onClick={onLogin}>Login</button>
+          </center> {/* ---<END CENTER>---------------------------------------- */}
+        </div> {/* ---<END grid-col_4 DIV>---------------------------------------- */}
+      </div> {/* ---<END grid-col_8 DIV>--------------------------------------------- */}
+    </div> //---<END container DIV>---------------------------------------------------- */}
+
+
+  ); //---<END RETURN>--------------------------------------------------------------------------------------- */}
+} //---<END LandingPage FUNCTION>------------------------------------------------------------------------------------ */}
 
 export default LandingPage;
