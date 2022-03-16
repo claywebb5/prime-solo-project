25bf0d1f7a4eabb13f3e4c811a1c26fdc6c3aedb
Clay Webb <clay_webb@outlook.com>
2022-03-16 14:48:41 -0500

    Created component for the TwelveSteps

---
+++ src/components/TwelveSteps/TwelveSteps.jsx
@@ -0,0 +1,13 @@
+// ========================<INTERACTIVE 12 STEP PROGRAM>====================
+import React from 'react';
+
+
+function TwelveSteps() {
+  return (
+    <div className="container">
+      <p>Interactive Twelve Step Program</p>
+    </div>
+  );
+}
+
+export default TwelveSteps;
