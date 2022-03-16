e8024657c1ade4983be6394985b831f69c859dc8
Clay Webb <clay_webb@outlook.com>
2022-03-16 16:50:11 -0500

    Installed necessary packages for the google maps API

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,14 @@
 // ========================<THIS IS THE MAP VIEW>====================
-import React from 'react';
+import React, { Component } from 'react';
+// To use the google maps JS API with react
+import GoogleMap from 'google-map-react';
+// To make POST requests to our backend server
+import axios from 'axios';
+// To enable realtime functionality with Pusher
+import Pusher from 'pusher-js';
+// To notify users when new users are online or go offline
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function MapView() {
