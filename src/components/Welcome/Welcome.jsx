2298e70c8228d4bd1280ec0f0fe32519bfa1c23d
Clay Webb <clay_webb@outlook.com>
2022-03-16 15:45:24 -0500

    Updated the imports on the Welcome component

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,16 +3,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import './Welcome.css';
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import { red } from "@material-ui/core/colors";
-// import FavoriteIcon from "@material-ui/icons/Favorite";
-// import ShareIcon from "@material-ui/icons/Share";
+
+
 
 
 function Welcome() {
