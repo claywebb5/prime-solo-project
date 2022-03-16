1cb35dcbae8ec8774f04cccbe6900c4fe4510f5e
Clay Webb <clay_webb@outlook.com>
2022-03-16 16:11:02 -0500

    Imported from MUI: Box, TextField, and Button onto the TwelveSteps component; Made a Box for the first step

--- src/components/TwelveSteps/TwelveSteps.jsx
+++ src/components/TwelveSteps/TwelveSteps.jsx
@@ -1,13 +1,248 @@
 // ========================<INTERACTIVE 12 STEP PROGRAM>====================
 import React from 'react';
-
+import './TwelveSteps.css'
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import { Button } from "@material-ui/core";
 
 function TwelveSteps() {
-  return (
-    <div className="container">
-      <p>Interactive Twelve Step Program</p>
-    </div>
-  );
+    return (
+        <>
+            <div className="container">
+                <p>Interactive Twelve Step Program</p>
+            </div>
+
+            {/* ============<START INTERACTIVE TWELVE STEP PROGRAM FORM>============= */}
+            
+            {/* =============<FIRST STEP>================ */}
+            <Box component="form">
+                <div>
+                    <h3>First Step</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+            </Box>
+
+                {/* =============< STEP>================ */}
+                <div>
+                    <h3> Step Label</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    {/* <Button onClick={handleSubmit}>Submit</Button>
+                    <Button onClick={handleReset}>Reset</Button> */}
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+                </Box>
+
+                {/* =============< STEP>================ */}
+                <div>
+                    <h3> Step Label</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    {/* <Button onClick={handleSubmit}>Submit</Button>
+                    <Button onClick={handleReset}>Reset</Button> */}
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+                </Box>
+
+                {/* =============< STEP>================ */}
+                <div>
+                    <h3> Step Label</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    {/* <Button onClick={handleSubmit}>Submit</Button>
+                    <Button onClick={handleReset}>Reset</Button> */}
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+                </Box>
+
+                {/* =============< STEP>================ */}
+                <div>
+                    <h3> Step Label</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    {/* <Button onClick={handleSubmit}>Submit</Button>
+                    <Button onClick={handleReset}>Reset</Button> */}
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+                </Box>
+
+                {/* =============< STEP>================ */}
+                <div>
+                    <h3> Step Label</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    {/* <Button onClick={handleSubmit}>Submit</Button>
+                    <Button onClick={handleReset}>Reset</Button> */}
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+                </Box>
+
+                {/* =============< STEP>================ */}
+                <div>
+                    <h3> Step Label</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    {/* <Button onClick={handleSubmit}>Submit</Button>
+                    <Button onClick={handleReset}>Reset</Button> */}
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+                </Box>
+
+                {/* =============< STEP>================ */}
+                <div>
+                    <h3> Step Label</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    {/* <Button onClick={handleSubmit}>Submit</Button>
+                    <Button onClick={handleReset}>Reset</Button> */}
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+                </Box>
+
+                {/* =============< STEP>================ */}
+                <div>
+                    <h3> Step Label</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    {/* <Button onClick={handleSubmit}>Submit</Button>
+                    <Button onClick={handleReset}>Reset</Button> */}
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+                </Box>
+
+                {/* =============< STEP>================ */}
+                <div>
+                    <h3> Step Label</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    {/* <Button onClick={handleSubmit}>Submit</Button>
+                    <Button onClick={handleReset}>Reset</Button> */}
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+                </Box>
+
+                {/* =============< STEP>================ */}
+                <div>
+                    <h3> Step Label</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    {/* <Button onClick={handleSubmit}>Submit</Button>
+                    <Button onClick={handleReset}>Reset</Button> */}
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+                </Box>
+
+                {/* =============< STEP>================ */}
+                <div>
+                    <h3> Step Label</h3>
+
+                    <p>*Actual step goes here*</p>
+
+                    <TextField
+                        // onChange={onTextChange}
+                        // value={textValue}
+                        label={"What you've done"} //optional
+                    />
+
+                    {/* <Button onClick={handleSubmit}>Submit</Button>
+                    <Button onClick={handleReset}>Reset</Button> */}
+                    <Button >Submit</Button>
+                    <Button >Reset</Button>
+                </div>
+            </Box>
+            {/* --------------<END INTERACTIVE TWELVE STEP PROGRAM FORM>--------------------- */}
+        </>
+    );
 }
 
 export default TwelveSteps;
