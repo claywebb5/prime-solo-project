e08b5a95b6ca2505b252fd40e17742d9dfcd22b5
Clay Webb <clay_webb@outlook.com>
2022-03-16 17:17:47 -0500

    Created a new pusher object in the lifecycle method

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -28,36 +28,84 @@ const imgStyle = {
 //---<LOCATION OF USER>------------------------
 const Marker = ({ title }) => (
     <div style={markerStyle}>
-      <img style={imgStyle} src="https://res.cloudinary.com/og-tech/image/upload/s--OpSJXuvZ--/v1545236805/map-marker_hfipes.png" alt={title} />
-      <h3>{title}</h3>
+        <img style={imgStyle} src="https://res.cloudinary.com/og-tech/image/upload/s--OpSJXuvZ--/v1545236805/map-marker_hfipes.png" alt={title} />
+        <h3>{title}</h3>
     </div>
-  );
+);
 
 // =================<CREATE A CLASS COMPONENT>=============================
 class MapView extends Component {
 
- //----<ADD STATES TO OUR COMPONENT TO TRACK>----------------------------------
- constructor(props) {
-    super(props)
-    this.state = {
-      // The map center
-      center: { lat: 5.6219868, lng: -0.23223 },
-      // Location for other online users
-      locations: {},
-      // Users currently online
-      users_online: {},
-      // Username of current user
-      current_user: ''
-    }
-  } //----<END STATE>-------------------------
+    //----<ADD STATES TO COMPONENT TO TRACK>----------------------------------
+    constructor(props) {
+        super(props)
+        this.state = {
+            // The map center
+            center: { lat: 5.6219868, lng: -0.23223 },
+            // Location for other online users
+            locations: {},
+            // Users currently online
+            users_online: {},
+            // Username of current user
+            current_user: ''
+        }
+    } //----<END STATE>-------
 
-  
-  
-  return (
-    // <div className="container">
-    //   <p>This is the map view</p>
-    // </div>
-  );
+    //----<LIFECYCLE METHOD>----------------------------------------
+    // Pusher object binding the front & back end listening for:
+    componentDidMount() {
+        let pusher = new Pusher("89062ebb8b009df054e1", {
+            authEndpoint: "http://localhost:3128/pusher/auth",
+            cluster: "us2"
+        })
+        // Subscribe to presence channel
+        this.presenceChannel = pusher.subscribe('presence-channel');
+
+        // Event triggered from the backend server when a user successfully
+        // subscribes to a presence channel
+        this.presenceChannel.bind('pusher:subscription_succeeded', members => {
+            this.setState({
+                users_online: members.members,
+                current_user: members.myID
+            });
+            this.getLocation();
+            this.notify();
+        })
+
+        // Event triggered when another user's location is updated
+        this.presenceChannel.bind('location-update', body => {
+            this.setState((prevState, props) => {
+                const newState = { ...prevState }
+                newState.locations[`${body.username}`] = body.location;
+                return newState;
+            });
+        });
+
+        // Event triggered when another user goes offline
+        this.presenceChannel.bind('pusher:member_removed', member => {
+            this.setState((prevState, props) => {
+                const newState = { ...prevState };
+                // remove member location once they go offline
+                delete newState.locations[`${member.id}`];
+                // delete member from the list of online users
+                delete newState.users_online[`${member.id}`];
+                return newState;
+            })
+            this.notify()
+        })
+
+        // Event triggered when a new user comes online
+        this.presenceChannel.bind('pusher:member_added', member => {
+            this.notify();
+        })
+
+    } //----<END LIFECYCLE METHOD>--------
+
+    return(
+        // <div className="container">
+        //   <p>This is the map view</p>
+        // </div>
+    );
 }
 
 export default MapView;
