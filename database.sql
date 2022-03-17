d4e80ba09e53a8493e78fb89e9c625e7c5d636a6
Clay Webb <clay_webb@outlook.com>
2022-03-17 14:11:58 -0500

    Updated database.sql file with the prayer table

--- database.sql
+++ database.sql
@@ -8,3 +8,24 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+-- Prayer table will be one (user) to many (prayer)
+CREATE TABLE "prayer" (
+    "id" SERIAL PRIMARY KEY,
+    "prayer" VARCHAR (1000),
+    "interpretation" VARCHAR(1000),
+    "user_id" INT REFERENCES "user"
+);
+
+-- Twelve Steps table..
+CREATE TABLE "twelve_steps" (
+    "id" SERIAL PRIMARY KEY,
+    ...
+);
+
+-- Location table will be..
+CREATE TABLE "location" (
+"id" SERIAL PRIMARY KEY,
+"user_id" INT REFERENCES "user",
+...
+);
