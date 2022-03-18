a276007b82fd51f2e8a51b02a951460ba7c90fda
Clay Webb <clay_webb@outlook.com>
2022-03-18 13:56:06 -0500

    Created a reducer for the selected prayer to be edited

---
+++ src/redux/reducers/selectedPrayer.reducer.js
@@ -0,0 +1,13 @@
+
+
+// Selected prayer reducer
+const selectedPrayer = (state = [], action) => {
+    switch (action.type) {
+        case 'SELECTED_PRAYER':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export default selectedPrayer;
