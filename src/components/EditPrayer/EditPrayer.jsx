84e19f380355b3068fd4e2d357c634582be4e1af
Clay Webb <clay_webb@outlook.com>
2022-03-23 03:32:24 -0500

    Working on EditPrayer component to edit the interpretation

--- src/components/EditPrayer/EditPrayer.jsx
+++ src/components/EditPrayer/EditPrayer.jsx
@@ -1,7 +1,12 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { Button } from "@material-ui/core";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Container } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
 
 
 function EditPrayer() {
@@ -9,34 +14,67 @@ function EditPrayer() {
     const history = useHistory();
     const dispatch = useDispatch();
 
+    // const [open, setOpen] = useState(false);
     const [newInterpretation, setNewInterpretation] = useState('');
+    // const [oldInterpretation, setOldInterpretation] = useState('');
 
     const prayer = useSelector(store => store.selectedPrayer)
 
-   function handleSubmit() {
-       alert(`Can't do that yet`);
-       setNewInterpretation('');
-   }
+    // const previous = () => {
+    //     setOpen(!open);
+    //     setOldInterpretation(prayer.interpretation);
+    // }
 
-    function handleReturn(){
+    const handleChange = (event) => {
+        // setNewInterpretation({...})
+
+    }
+
+    // const handleClose = () => {
+    //     if (open === true) {
+    //         setOpen(!open);
+    //     } else {
+    //         console.log('Nope');
+    //         console.log('Open is:', open);
+    //     }
+    // }
+
+    function handleSubmit() {
+        
+    }
+
+    function handleReturn() {
         history.push('/welcome');
     }
     return (
         <>
-            <h1>Prayer Edit</h1>
-            <div key={prayer.id}>
-                <h3>{prayer.prayer_name}</h3>
-                <h4>"{prayer.prayer_text}"</h4>
+            <Container>
+                <h1>Prayer Edit</h1>
+                <div key={prayer.id}>
+                    <h3>{prayer.prayer_name}</h3>
+                    <h4>"{prayer.prayer_text}"</h4>
 
-                <p>Interpretation:</p>
-                <input
-                    value={newInterpretation}
-                    onChange={evt => setNewInterpretation(evt.target.value)}
-                />
-                <button onClick={handleSubmit}>Submit</button>
-            </div>
-            
-            <Button variant="contained" onClick={handleReturn}>Home</Button>
+                    <p>Interpretation:</p>
+                    <input
+                        value={newInterpretation}
+                        onChange={handleChange}
+                    />
+                    <button onClick={handleSubmit}>Submit</button>
+                </div>
+            </Container>
+            {/* <Container>
+                <Button variant="contained" onClick={previous}>See Previous</Button>
+            </Container>
+            <Card open={open}>
+                {oldInterpretation}
+                <CardActions>
+                    <Button onClick={handleClose}>Close</Button>
+                </CardActions>
+            </Card> */}
+
+            <Container>
+                <Button variant="contained" onClick={handleReturn}>Home</Button>
+            </Container>
             {/* <h1>** ADD TO FAVORITE LIST OF PRAYERS **</h1> */}
         </>
     )
