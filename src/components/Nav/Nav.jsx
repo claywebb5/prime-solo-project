c6f1a048c7306725d730c20011fec3df451e7cc6
Clay Webb <clay_webb@outlook.com>
2022-07-15 20:12:36 -0500

    Uninstalled MUI core; Added the App Bar to the Nav and menu icon for links to the other views in the app

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -18,99 +18,166 @@ import { styled } from '@mui/material/styles';
 
 
 function Nav() {
-  // ========< TOOLS >==============
-  const history = useHistory();
-  const user = useSelector((store) => store.user);
-  const dispatch = useDispatch();
+    // ========< TOOLS >==============
+    const history = useHistory();
+    const user = useSelector((store) => store.user);
+    const dispatch = useDispatch();
 
-  // =====< USESTATE >=============================
-  // For the hamburger icon menu
-  const [anchorElMenu, setAnchorElMenu] = useState(null);
-  // =====< CLICK LISTENERS >=============================
-  // OPEN the hamburger icon menu
-  const handleOpenMenu = (event) => {
-    setAnchorElMenu(event.currentTarget);
-  };
-  // CLOSE the hamburger icon menu
-  const handleCloseMenu = () => {
-    setAnchorElMenu(null);
-  };
-  // GO Home
-  const handleHome = () => {
-    history.push("/welcome")
-  };
-  // GO to 12 Steps
-  const handleSteps = () => {
-    history.push("/steps")
-  };
-  // GO to Map
-  const handleMap = () => {
-    history.push("/map")
-  };
-  // GO to About
-  const handleAbout = () => {
-    history.push("/about")
-  };
-  // GO to Profile
-  const handleProfile = () => {
-    history.push("/user")
-  };
-  
+    // =====< USESTATE >=============================
+    // For the hamburger icon menu
+    const [anchorElMenu, setAnchorElMenu] = useState(null);
+    // =====< CLICK LISTENERS >=============================
+    // OPEN the hamburger icon menu
+    const handleOpenMenu = (event) => {
+        setAnchorElMenu(event.currentTarget);
+    };
+    // CLOSE the hamburger icon menu
+    const handleCloseMenu = () => {
+        setAnchorElMenu(null);
+    };
+    // GO Home
+    const handleHome = () => {
+        history.push("/welcome")
+    };
+    // GO to 12 Steps
+    const handleSteps = () => {
+        history.push("/steps")
+    };
+    // GO to Map
+    const handleMap = () => {
+        history.push("/map")
+    };
+    // GO to About
+    const handleAbout = () => {
+        history.push("/about")
+    };
+    // GO to Profile
+    const handleProfile = () => {
+        history.push("/user")
+    };
+    // SIGN OUT
+    const handleSignOut = () => {
+        dispatch({ type: 'LOGOUT' });
+        history.push('/welcome');
+    };
 
-  return (
-    <div className="nav">
 
-      {/* <ON LOGIN / REGISTER VIEW - WHEN CLICKED BRINGS USER TO LANDING PAGE> */}
-      <Link className="navTitle" to="/home">
-        Hey Bill W.
-      </Link>
-      {/* ---------------------------------------------------------- */}
+    return (
+        <>
+            {/* <AppBar position="fixed" sx={{ marginBottom: 1 }}> */}
+            <AppBar sx={{ marginBottom: 1 }}>
 
-      <div>
+                {/* ------< HAMBURGER ICON >--------------- */}
+                <Box sx={{ flexGrow: 1 }}>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        aria-label="menu"
+                        aria-controls="menu-appbar"
+                        onClick={handleOpenMenu}
+                        sx={{ mr: 2, color: "#ace23a" }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu
+                        sx={{ mt: '45px' }}
+                        id="menu-appbar"
+                        anchorEl={anchorElMenu}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right', // <---- Where the drop down
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right', // <---- Where the drop down
+                        }}
+                        open={Boolean(anchorElMenu)}
+                        onClose={handleCloseMenu}
+                    >
+                        <MenuItem onClick={handleCloseMenu}>
+                            <Typography textAlign="center" onClick={handleProfile}>Profile</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={handleCloseMenu}>
+                            <Typography textAlign="center" onClick={handleSteps}>Steps</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={handleCloseMenu}>
+                            <Typography textAlign="center" onClick={handleMap}>Map</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={handleCloseMenu}>
+                            <Typography textAlign="center" onClick={handleAbout}>About</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={handleCloseMenu}>
+                            <Typography textAlign="center" onClick={handleSignOut}>Sign Out</Typography>
+                        </MenuItem>
+                    </Menu>
+                </Box>
+                {/* ------< LOGO ICON >--------------- */}
+                <Box sx={{ flexGrow: 1 }}>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        aria-label="logo"
+                        aria-controls="logo-appbar"
+                        onClick={handleHome}
+                        sx={{ mr: 2 }}
+                    >
 
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
+                    </IconButton>
+                </Box>
+            </AppBar>
+            <div className="nav">
 
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            {/* <WELCOME VIEW> */}
-            <Link className="navLink" to="/welcome">
-              Home
-            </Link>
+                {/* <ON LOGIN / REGISTER VIEW - WHEN CLICKED BRINGS USER TO LANDING PAGE> */}
+                <Link className="navTitle" to="/home">
+                    Hey Bill W.
+                </Link>
+                {/* ---------------------------------------------------------- */}
 
-            {/* <THIS WILL BE THE INTERACTIVE 12 STEP PROGRAM> */}
-            {/* <USED TO BE THE LINK FOR INFO> */}
-            <Link className="navLink" to="/steps">
-              12 Steps
-            </Link>
+                <div>
 
-            {/* <THIS WILL BE THE MAP> */}
-            <Link className="navLink" to="/map">
-              Map
-            </Link>
+                    {/* If no user is logged in, show these links */}
+                    {!user.id && (
+                        // If there's no user, show login/registration links
+                        <Link className="navLink" to="/login">
+                            Login / Register
+                        </Link>
+                    )}
 
-            {/* <USER PAGE & LOG OUT BUTTON COMPONENTS> */}
-            {/* <Link className="navLink" to="/user">
-              Profile
-            </Link> */}
+                    {/* If a user is logged in, show these links */}
+                    {user.id && (
+                        <>
+                            {/* <WELCOME VIEW> */}
+                            <Link className="navLink" to="/welcome">
+                                Home
+                            </Link>
 
-          </>
-        )}
-        {/* <THIS WILL BE THE ABOUT PAGE COMPONENT> */}
-        <Link className="navLink" to="/about">
-          About
-        </Link>
+                            {/* <THIS WILL BE THE INTERACTIVE 12 STEP PROGRAM> */}
+                            {/* <USED TO BE THE LINK FOR INFO> */}
+                            <Link className="navLink" to="/steps">
+                                12 Steps
+                            </Link>
 
-      </div>
+                            {/* <THIS WILL BE THE MAP> */}
+                            <Link className="navLink" to="/map">
+                                Map
+                            </Link>
 
-    </div>
-  );
+                            {/* <USER PAGE & LOG OUT BUTTON COMPONENTS> */}
+                            {/* <Link className="navLink" to="/user">Profile</Link> */}
+
+                        </>
+                    )}
+                    {/* <THIS WILL BE THE ABOUT PAGE COMPONENT> */}
+                    <Link className="navLink" to="/about">
+                        About
+                    </Link>
+
+                </div>
+
+            </div>
+        </>
+    );
 }
 
 export default Nav;
